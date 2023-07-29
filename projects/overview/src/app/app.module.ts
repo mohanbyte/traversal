a8ce12910de2befbb546f30d7c220083f9af57b5
Mohan <kmohan0910@gmail.com>
2023-07-29 22:38:16 +0530

    intial commit

--- projects/overview/src/app/app.module.ts
+++ projects/overview/src/app/app.module.ts
@@ -3,17 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
-const path: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('overview/Component').then((m) => m.OverviewModule),
-  },
-];
+import { OverviewModule } from './overview/overview.module';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(path)],
+  imports: [BrowserModule, AppRoutingModule, OverviewModule],
   providers: [],
   bootstrap: [AppComponent],
 })
