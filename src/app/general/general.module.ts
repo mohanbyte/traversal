63d221809ab5b7b7b53ba68e72c5160218aa7852
Mohan <kmohan0910@gmail.com>
2024-04-13 20:25:46 +0530

    updated to experimental material

--- src/app/general/general.module.ts
+++ src/app/general/general.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PinDialogComponent } from '../dashboard/pin-dialog/pin-dialog.component';
 import { MaterialModule } from '../material.module';
 
 @NgModule({
