e46fe88a0f459dd2324f8a4ebcce25b6e1d21965
= <kmohan0910@gmail.com>
2024-06-19 21:51:42 +0530

    Added Firebase and updated default routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [{
-  path: 'login',
-  component: LoginComponent
-},
-{
-  path:'dashboard',
-  component: DashboardComponent
-},
-{path: '**',
-redirectTo: 'login'}];
+const routes: Routes = [
+  //   {
+  //   path: 'login',
+  //   component: LoginComponent
+  // },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  { path: '**', redirectTo: 'dashboard' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
